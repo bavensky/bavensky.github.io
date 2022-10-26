097916f8eeacac274a99411e92244ced54605850
bavensky <apiruk_sang-ngenchai@hotmail.com>
2022-10-26 14:32:33 +0900

    color picker

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
-var obniz = new Obniz("OBNIZ_ID_HERE");
-obniz.onconnect = async function() {
-  var led = obniz.wired("WS2811", { din: 3, vcc: 2, gnd: 1 });
-  led.rgb(0x33, 0x33, 0);
-
-  $("#colorPicker").on("change", function(val) {
-    let colorString = $("#colorPicker").val();
-
-    let rgb = colorString2hex(colorString);
-    led.rgb(rgb[0], rgb[1], rgb[2]);
-  });
-};
-
-function colorString2hex(hex) {
-  if (hex.slice(0, 1) == "#") hex = hex.slice(1);
-  if (hex.length == 3)
-    hex =
-      hex.slice(0, 1) +
-      hex.slice(0, 1) +
-      hex.slice(1, 2) +
-      hex.slice(1, 2) +
-      hex.slice(2, 3) +
-      hex.slice(2, 3);
-
-  return [hex.slice(0, 2), hex.slice(2, 4), hex.slice(4, 6)].map(function(str) {
-    return parseInt(str, 16);
-  });
-}
+let red = document.getElementById('red');
+let green = document.getElementById('green');
+let blue = document.getElementById('blue');
+let box = document.querySelector('div.neumorphism-3');
+  
+let r = 0, g = 0, b = 0;
+  
+red.addEventListener("keyup", function (event) {
+    r = red.value;
+    if (!r)
+        r = 0;
+    box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
+  
+green.addEventListener("keyup", function (event) {
+    g = green.value;
+    if (!g)
+        g = 0;
+    box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
+  
+blue.addEventListener("keyup", function (event) {
+    b = blue.value;
+    if (!b)
+        b = 0;
+    box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
